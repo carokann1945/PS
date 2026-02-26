8d6527392418d49d8aeb179bac37f706c237602c
carokann1945 <carokann1945@gmail.com>
2026-02-27 04:28:37 +0900

    스택 & 큐 구현하기

---
+++ 스택 & 큐 구현하기/stack.js
@@ -0,0 +1,24 @@
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+  push(value) {
+    this.array.push(value);
+  }
+  pop() {
+    return this.array.pop();
+  }
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+}
+
+const stack = new Stack();
+stack.push("A");
+stack.push("B");
+stack.push("C");
+stack.push("D");
+console.log("stack", stack);
+stack.pop();
+console.log("stack", stack);
+console.log("stack", stack.peek());
